438efb64b78243f0b1dc699298b48c4f3ab72efd
DianaMasgras <dianamadalina30@gmail.com>
2022-03-03 17:04:27 +0200

    display products on frontend

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/products", (res) => {
+app.use(function (req, res, next) {
+  const allowedOrigins = ['http://localhost:3000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, DELETE, PUT');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+
+  return next();
+});
+
+app.get("/products", (req, res) => {
   console.log("Connected to React");
   res.send([{ name: 'name1' }, { name: 'name2' }]);
 });
